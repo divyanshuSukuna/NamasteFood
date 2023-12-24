c2539c543de3f61de3e6f5302001bd5c5dd0b44e
divyanshuSukuna <divyanshugd47@gmail.com>
2023-12-25 00:29:07 +0530

    React Routing

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div className="about">
+      <h1>About</h1>
+      <h3>This is Namaste React</h3>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1 className="contact-page">Contact Us Page</h1>
+      <h3>Email</h3>
+      <h3>Phone</h3>
+    </div>
+  );
+};
+
+export default Contact;
